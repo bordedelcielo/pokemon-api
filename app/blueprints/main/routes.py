2641c98adbfa6f1678bce06c40007aab199edc8f
bordedelcielo <christopher.thrutchley@gmail.com>
2022-01-26 20:23:51 -0600

    first commit

---
+++ app/blueprints/main/routes.py
@@ -0,0 +1,8 @@
+from flask import render_template, request
+import requests
+from .import bp as main
+
+@main.route('/', methods = ['GET'])
+
+def index():
+    return render_template('index.html.j2')
