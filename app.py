71d1c31d629e5b049707ff46f680cc7e0e156915
bordedelcielo <christopher.thrutchley@gmail.com>
2022-01-26 20:32:40 -0600

    first commit

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
 from app import create_app
 
-from flask import Flask
-
-def create_app(config_filename):
-    app = Flask(__name__)
-    app.config.from_pyfile(config_filename)
-
-    
-
-    return app
+app = create_app()
